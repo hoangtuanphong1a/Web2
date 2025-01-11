d1159b1e8e953a7d1a74fe85384e0074ec94d14d
hoangtuanphong1a <hoangtuanphong1a@gmail.com>
2025-01-11 20:02:13 +0700

    update day1

---
+++ Day01/src/main/java/com/phong/day01/model/User.java
@@ -0,0 +1,58 @@
+package com.phong.day01.model;
+
+public class User {
+    private Long id;
+    private String name;
+    private int age;
+    private String email;
+    private String address;
+
+    public User(Long id, String name, int age, String email, String address) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.address = address;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
