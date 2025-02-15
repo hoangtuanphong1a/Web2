a183ee9a1d2e82a7afc0743fb12588abdb862313
levanvutru358 <128678254+levanvutru358@users.noreply.github.com>
2025-02-15 14:25:19 +0700

    Delete day3 directory

--- day3/src/main/java/com/example/day3/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.day3.repository;
-
-import com.example.day3.model.UserDemo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<UserDemo, Integer> {
-}
